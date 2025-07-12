6a073507b1a6c4e294bb0fb5fac6d6c0d2b0dd15
Keith <kpost342005@hotmail.com>
2025-07-12 17:27:42 -0400

    Included dyanmic artist carousel, simplified UI layout, and developed server code to output artist name, bio, bio caption, and carousel

--- modules/02_art_explore_tab.R
+++ modules/02_art_explore_tab.R
@@ -50,22 +50,16 @@ exploreUI <- function(id) {
     
     ## Outputs
     fluidRow(
-      h3(strong(textOutput(ns("out_txt_artist")))),
-    ),
-    fluidRow(
-      column(4,
-        p(textOutput(ns("out_txt_bio"))),
-        tags$p(textOutput(ns("out_txt_citation")),
-          style = "font-size: 0.8em; color: gray; font-style: italic;")
+      column(6, 
+        #artist name, bio, and citation
+        h3(strong(textOutput(ns("out_txt_artist")))),
+        p(uiOutput(ns("out_txt_bio"))),
+        p(uiOutput(ns("out_txt_citation")))
       ),
       column(1),
-      column(3,
-        h4(textOutput(ns("out_txt_bio_title"))),
-        p(textOutput(ns("out_txt_bio_body")))
-      ),
-      column(4,
-        slickROutput(ns("carousel_sample"))
-        # imageOutput(ns("out_img_artwork"))
+      column(5,
+        #carousel of artwork with caption below
+        slickROutput(ns("carousel_artwork"))
       )
     )
   )
@@ -80,118 +74,154 @@ exploreServer <- function(id) {
   moduleServer(id, function(input, output, session) {
   
 
-  ## Update dropdown menus
-  ### Create reactive of DF
-  df_art <- reactive({
-    df_art_info_full
-  })
-  # df_artist <- reactive({
-  #   df_artist_info
-  #   #note: later replace with connection to API
-  # })
-
-
-  observeEvent(input$sel_movement, {
-    req(input$sel_movement)
-    df_filt <- filter(df_art(), movement==input$sel_movement)
+    ## Update dropdown menus
+    ### Create reactive of DF
+    df_art <- reactive({
+      df_art_info_full
+    })
+  
+  
+    ### Update dropdowns
+    #movement
+    observeEvent(input$sel_movement, {
+      req(input$sel_movement)
+      df_filt <- filter(df_art(), movement==input$sel_movement)
+        
+      vec_natl_update <- unique(df_filt$nationality)
+  
+      vec_artist1_update <- unique(df_filt$artist)
+  
+      updateSelectInput(session,
+                        'sel_movement',
+                        selected=input$sel_movement
+      )
+  
+      updateSelectInput(session,
+                        'sel_nationality',
+                        choices=c("Select one"="", vec_natl_update)
+      )
+  
+      updateSelectInput(session,
+                        'sel_artist',
+                        choices=c("Select one"="", vec_artist1_update)
+      )
+    })
+  
+    #nationality
+    observeEvent(input$sel_nationality, {
+      req(input$sel_nationality)
+      df_filt <- filter(df_art(), nationality==input$sel_nationality)
+  
+      vec_artist2_update <- unique(df_filt$artist)
+  
+  
+      updateSelectInput(session,
+                        'sel_nationality',
+                        selected=input$nationality
+      )
+  
+      updateSelectInput(session,
+                        'sel_artist',
+                        choices=c("Select one"="", vec_artist2_update)
+      )
+    })
+    
+    
+    ## Create selected artist reactive DF
+    df_art_sel <- reactive({
+      req(input$sel_artist)
       
-    vec_natl_update <- unique(df_filt$nationality)
-
-    vec_artist1_update <- unique(df_filt$artist)
-
-    updateSelectInput(session,
-                      'sel_movement',
-                      selected=input$sel_movement
-    )
-
-    updateSelectInput(session,
-                      'sel_nationality',
-                      choices=c("Select one"="", vec_natl_update)
-    )
-
-    updateSelectInput(session,
-                      'sel_artist',
-                      choices=c("Select one"="", vec_artist1_update)
-    )
-  })
-
-  observeEvent(input$sel_nationality, {
-    req(input$sel_nationality)
-    df_filt <- filter(df_art(), nationality==input$sel_nationality)
-
-    vec_artist2_update <- unique(df_filt$artist)
-
-
-    updateSelectInput(session,
-                      'sel_nationality',
-                      selected=input$nationality
-    )
-
-    updateSelectInput(session,
-                      'sel_artist',
-                      choices=c("Select one"="", vec_artist2_update)
-    )
-  })
-
-  
-  ## Artist Name
-  ### Render output
-  output$out_txt_artist <- renderText({
-    format_name(input$sel_artist)
-  })
-  
-  
-  ## Artist bio
-  ### Create reactive
-  txt_bio <- reactive({
-    req(input$sel_artist)
-    df_art() %>%
-      filter(artist==input$sel_artist) %>%
-      pull(bio) %>%
-      unique()
-  })
-  
-  
-  ### Render output
-  output$out_txt_bio <- renderText({
-    txt_bio()
-  })
-  
-  output$out_txt_citation <- renderText({
-    req(input$sel_artist)
-    "Bio generated with the assistance of ChatGPT (OpenAI, 2025)."
-  })
+      df_art() %>%
+        filter(artist==input$sel_artist) 
+    })
   
     
+    ## Artist Name
+    ### Render output
+    output$out_txt_artist <- renderText({
+      format_name(input$sel_artist)
+    })
     
     
-  ## Image rendering/display
-  # output$img_artist <- renderImage({
-  #   req(input$file_img)
-  #   if(is.null(input$file_img)) {
-  #     return(NULL)
-  #   }
-  # 
-  #   file_path <- input$file_img$datapath
-  # 
-  #   list(src = file_path,
-  #        alt = "Uploaded Image")
-  # }, deleteFile = FALSE)
+    ## Artist bio
+    ### Create reactive
+    txt_bio <- reactive({
+      req(input$sel_artist)
+      
+      df_art_sel() %>%
+        filter(artist==input$sel_artist) %>%
+        pull(bio) %>%
+        unique()
+    })
+    
+    
+    ### Render output
+    #bio
+    output$out_txt_bio <- renderUI({
+      markdown(txt_bio())
+    })
+    
+    #citation
+    output$out_txt_citation <- renderUI({
+      req(input$sel_artist)
+      em("Bio generated with the assistance of ChatGPT (OpenAI, 2025).")
+    })
+    
+      
+    ## Image rendering/display
+    ### Create reactive of urls
+    df_art_sel_info <- reactive({
+      req(input$sel_artist)
+      
+      mvmt <- df_art_sel()$movement[1]
+      
+      df_art_sel() %>%
+        filter(movement==mvmt) %>%
+        slice_sample(n=3) %>%
+        select(artist_simple, title, date, image_url) 
+    })
+    
+    
+    ## Artworks
+    output$carousel_artwork <- renderSlickR({
+      #create vectors for building carousel
+      imgs <- df_art_sel_info() %>% pull(image_url)
+      
+      artist <- df_art_sel_info() %>% pull(artist_simple)
+      
+      titles <- df_art_sel_info() %>% pull(title)
+      
+      dates <- df_art_sel_info() %>% 
+        pull(date) %>%
+        paste0("(", ., "),")
+      
+      closing <- "The Metropolitan Museum of Art. Open Access. Image courtesy of The Met."
+        
+      #combine vectors to generate caption vector
+      captions <- paste(" by", 
+                       artist,
+                       dates,
+                       closing)
+      
+      #build slides for carousel
+      slides <- purrr::pmap(list(imgs, titles, captions), function(x, y, z) {
+        tags$div(
+          style = "text-align: center;",
+          tags$img(
+            src = x,
+            style = "max-width: 100%; height: 300px; display: block; margin: auto;"
+          ),
+          tags$p(
+            tags$em(y),
+            z, 
+            style = "margin-top: 10px; font-size: 14px; color: #444;")
+        )
+      })
+       
+      #generate carousel
+      slickR(slides)
   
-  
-  ## Artworks
-  output$carousel_sample <- renderSlickR({
-    req(input$sel_artist)
-    # Provide image URLs or file paths
-    imgs <- list(
-      img(src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Shaki_waterfall.jpg/640px-Shaki_waterfall.jpg", height = "300px"),
-      img(src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Nepal_Mount_Everest.jpg/640px-Nepal_Mount_Everest.jpg", height = "300px"),
-      img(src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_in_Paris.jpg/640px-Eiffel_Tower_in_Paris.jpg", height = "300px")
-    )
-    slickR(imgs)
-  })
-    
-    
+    })
   })
 }
 
