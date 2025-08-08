06f160870acfe98a968fb7bf05cf3b6b6e203f4f
Keith <kpost342005@hotmail.com>
2025-08-07 21:43:02 -0400

    Developed code to access, verify, and store metadata related to The Met's artwork followed by code to filter and clean the data

--- backbone/01_art-id-ml.R
+++ backbone/01_art-id-ml.R
@@ -8,7 +8,7 @@
 
 
 # Load Packages and Functions=======================================================================
-pacman::p_load(here, httr, jsonlite, magick)
+pacman::p_load(here, tidyverse, httr, jsonlite, magick, skimr, visdat, stringi, janitor)
 
 source(here("fns_objs", "00_fn.R"))
 
@@ -16,39 +16,288 @@ source(here("fns_objs", "00_fn.R"))
 
 # 1. Image Data Collection==========================================================================
 # Goal: Gather a dataset of images that will be used to train the machine learning model.
-# Considerations:
-# You need a collection of labeled images (if you're doing supervised learning) or unlabeled images 
-  #(for unsupervised learning).
-# The images should be relevant to your problem (e.g., artwork images, medical images, etc.).
-# Example: If you're working with artwork, collect images of different artwork categories, such as 
-  #paintings, sketches, or digital art.
+
+## Step 1: Get the object IDs of possible artworks (remember that some don't have valid URLs) & 
+  #split into reasonable sizes for next step
+### Grab all object IDs
+vec_art_objs <- search_paintings(nationality="Europe", public=NULL) %>% unlist()
+length(vec_art_objs) #12697
 
 
-## Step 1: Get the object IDs of possible artworks (remember that some don't have valid URLs)
-natls <- c("North America", "Europe")
+### Split up vector
+#### Calculate processing time
+length(vec_art_objs) * 1.5 #19045.5 sec
+19045.5/60 #~317 min
 
-vec_art_objs <- purrr::map(natls, search_paintings) %>%
-  unlist() 
+length(vec_art_objs)/50 #~254 groups of 50 obj IDs
 
+254 * 30 #7620 seconds of breaks between groups
+7620/60 #127 minutes
 
-## Step 2: Retain object IDs that have valid URLs and non-null values for artist name and artwork
-  #(first to see if there's enough art to train a model for both)
-## Run function
-obj <- vec_art_objs[1]
-
-obj <- validate_obj_id(obj)
-
-valid_obj_id <- purrr:::map_int(vec_art_objs[1:1000], validate_obj_id) %>%
-  .[!is.na(.)]
+317 + 127 #444 min
+444/60 #7.4 hours
 
 
-# Get information associated with an object ID
-## Define the search URL for the Metropolitan Museum API
-obj1 <- vec_art_objs[1]
+#### Split up obj id vector and save
+#splitting
+grp <- 1:8
+indexes_end <- grp * 1750
+indexes_start <- indexes_end - 1749
+indexes_end[length(grp)] <- length(vec_art_objs)
 
-get_artwork_info(vec_art_objs[20])
 
-vec_art_info <- purrr::map(vec_art_objs, get_artwork_info)
+t_vec_art_objs <- purrr::map2(indexes_start, indexes_end, function(x, y) {
+    vec_art_objs[x:y]
+}) %>%
+  set_names((paste0("vec_art_obj", grp)))
+
+
+#saving
+# purrr::map(grp, function(x) {
+#   filename <- paste0("vec_art_objs", x, ".rds")
+#   filepath <- here("data", "obj_id_vecs", filename)
+# 
+#   vec <- t_vec_art_objs[[x]]
+# 
+#   saveRDS(vec, filepath)
+# })
+
+
+
+## Step 2: Retain object IDs & associated info that have valid URLs and non-null values for 
+  #artist name 
+## Develop function
+loop_get_artwork_info <- function(fileno){
+  # Read in obj ids
+  filename <- paste0("vec_art_objs", fileno, ".rds")
+  fp <- here("data", "obj_id_vecs", filename)
+  
+  vec_art_loop <- readRDS(fp)
+
+  # Create and populate list
+  t_art_full <- list()
+  n <- 1
+
+  for(i in seq(1, 1750, 50)){
+    t_art_loop <- purrr::map(vec_art_loop[i:(i+49)], function(obj_id) {
+      Sys.sleep(1.5 + runif(1, 0, 1))
+      get_artwork_info(obj_id)
+    })
+    Sys.sleep(30)
+    t_art_full <- c(t_art_full, t_art_loop)
+    print(paste("Batch", n, "Completed"))
+    n <- n + 1
+  }
+
+  return(t_art_full)
+
+}
+
+## Execute function
+filenum <- 8
+
+# t_obj <- loop_get_artwork_info(fileno=filenum)
+
+
+## Convert to DF and save
+# df_obj <- t_obj %>%
+#   compact() %>% #remove NULL entries
+#   bind_rows()
+# 
+# fname_df <- paste0("df_art_info", filenum, ".rds")
+# fp_out <- here("data", "art_info_dfs", fname_df)
+
+# saveRDS(df_obj, fp_out)
+
+
+## Step 3: Read in DFs of object IDs, combine, and save as one RDS
+vec_fn_art_info <- list.files(here("data", "art_info_dfs"), full.names=TRUE)
+
+df_art_info_all <- purrr::map_df(vec_fn_art_info, readRDS) %>%
+  select(!public)
+
+
+## Step 4: Combine with the public DF
+### Read in public art info DF
+fp_art_explore <- list.files(here("data"), "^02_art-exploration", full.names=TRUE) %>% 
+  sort(decreasing=TRUE)
+
+df_art_info_public <- readRDS(fp_art_explore)
+
+
+#### Join by object_id & filter down
+#create classification filter
+valid_classifications <- c(
+  "Paintings",
+  "Paintings-Canvas",
+  "Paintings-Fresco",
+  "Paintings-Frescoes",
+  "Paintings-Panels",
+  "Paintings-Icons",
+  "Miscellaneous-Paintings & Portraits"
+)
+
+
+
+# Helper function to lowercase common particles (you may already have this)
+clean_artist_names <- function(df, name_col = "artist_simple") {
+  
+  lowercase_particles <- function(name) {
+    # Fix apostrophe spacing: "d' Hondecoeter" -> "d'Hondecoeter"
+    name <- str_replace_all(name, regex("d'\\s+", ignore_case = TRUE), "d'")
+    
+    # Define particles to keep lowercase (added "der")
+    particles <- c("van", "von", "de", "del", "della", "da", "di", "le", "la", "du", 
+                   "des", "dos", "das", "den", "het", "op", "te", "ten", "ter", "der", "d'")
+    
+    # Split into parts
+    parts <- str_split(name, "\\s+")[[1]]
+    
+    fixed_parts <- sapply(parts, function(part) {
+      lower_part <- str_to_lower(part)
+      
+      # Handle d' prefix separately
+      if (str_starts(lower_part, "d'")) {
+        # lowercase 'd'', capitalize first letter after apostrophe + rest as-is
+        paste0("d'", str_to_upper(substr(part, 3, 3)), substr(part, 4, nchar(part)))
+      } else if (lower_part %in% particles) {
+        lower_part
+      } else {
+        # Capitalize first letter, keep rest as is
+        paste0(str_to_upper(substr(part, 1, 1)), substr(part, 2, nchar(part)))
+      }
+    })
+    
+    paste(fixed_parts, collapse = " ")
+  }
+  
+  df %>%
+    mutate(
+      artist_clean = !!sym(name_col) %>%
+        str_remove("\\s*\\(.*?\\)") %>%
+        str_remove_all("(?i)\\s+and workshop|\\s+called.*?$") %>%
+        str_remove_all("(?i)^Attributed To\\s*") %>%
+        str_remove_all("(?i)\\bthe elder\\b|\\bthe younger\\b|\\bthe third\\b") %>%
+        str_squish() %>%
+        str_replace_all("\\s*-\\s*", "-") %>%
+        stri_trans_general("Latin-ASCII") %>%  # remove accents
+        str_to_title() %>%
+        str_replace_all("[^\\w\\s\\.\\-']", "") %>%
+        str_replace_all("\\bD'", "d'") %>%
+        str_replace_all(" Y ", " y ") %>%
+        str_replace_all("Iii", "III") %>%
+        str_squish()
+    ) %>%
+    rowwise() %>%
+    mutate(
+      artist_clean = lowercase_particles(artist_clean)
+    ) %>%
+    ungroup() %>%
+    mutate(
+      artist_clean = str_squish(artist_clean),
+      artist_clean = str_remove(artist_clean, "\\s[\\p{Han}\\p{Hiragana}\\p{Katakana}]+$"),
+      artist_clean = str_remove(artist_clean, "(?i)^Sir\\s+|^Count\\s+"),
+      artist_clean = str_trim(artist_clean)
+    )
+}
+
+
+artist_names_anon <- c(
+  "^Master",
+  "^Maestro",
+  # "Master [Oo]f [Tt]he Saint Barbara Legend",
+  # "Master [Oo]f [Tt]he Saint Catherine Legend",
+  # "Master [Oo]f [Tt]he Saint Godelieve Legend",
+  # "Master [Oo]f [Tt]he Saint Ursula Legend",
+  # "Master [Oo]f [Tt]he Story [Oo]f Joseph",
+  # "Master [Oo]f [Tt]he View [Oo]f Saint Gudula",
+  # "Master [Oo]f [Tt]he Virgin [Aa]mong Virgins",
+  # "Master [Oo]f Varlungo",
+  "Monogrammist F[Aa]",
+  "Monogrammist F[Ss]",
+  "Monogrammist I[Ss]",
+  "Monogrammist J[Gg]",
+  "Monogrammist L[Aa][Mm]",
+  # "Master Morata",
+  "Morata Master",
+  "Osma Master",
+  "Osservanza Master",
+  "Netherlandish",
+  "Italian, Neapolitan Follower of Giotto",
+  "Master G.Z.",
+  "Follower of Lippo Memmi",
+  "Italian, Lombard \\(probably Pavia\\)",
+  "^German$",
+  "^Shosai$", #multiple artists
+  "Tosa School",
+  "Unidentified",
+  "Various Scholars",
+  "Byzantine or Crusader",
+  "Budapest Master",
+  "^Workshop of",
+  "[Pp]ainter",
+  "artist$",
+  "artists$",
+  "artist\\(s\\)$"
+)
+
+
+
+  
+# df_art_info_all_public <- 
+df_art_info_all %>%
+  #pull in True value of public field
+  left_join(df_art_info_public[,c("object_id", "public")], by="object_id") %>% 
+  mutate(public=replace_na(public, FALSE)) %>% 
+  #retain paintings & named artists
+  filter(classification %in% valid_classifications) %>% 
+  filter(!str_detect(artist_simple, "[Pp]ainter|artist$|artists$|artist\\(s\\)$")) %>% 
+  filter(!str_detect(artist_simple, paste(artist_names_anon, collapse="|"))) %>%
+  #identify NAs
+  mutate(across(.cols=c(nationality, bio, date, medium, dimensions, period, classification),
+                .fns=~na_if(.x, ""))) %>% 
+  #normalize artist names
+  clean_artist_names() %>% 
+  # filter(str_detect(artist_clean, "^Workshop")) %>% View()
+  pull(artist_clean) %>% unique() %>% sort() -> vec_artists
+
+
+# Load required package
+install.packages("stringdist")  # Run this only once
+library(stringdist)
+
+# Paste your full list of artist names here
+artists <- vec_artists
+
+# Compute a distance matrix (Jaro-Winkler is good for names)
+dist_matrix <- stringdistmatrix(artists, artists, method = "jw")
+
+# Convert to data frame and extract potential duplicates
+threshold <- 0.1  # lower threshold = more strict matching
+duplicates <- which(dist_matrix < threshold & dist_matrix > 0, arr.ind = TRUE)
+
+# Remove mirrored duplicates and self-pairs
+duplicates <- duplicates[duplicates[,1] < duplicates[,2], ]
+
+# Output similar pairs
+for (i in seq_len(nrow(duplicates))) {
+  cat(
+    sprintf("[%d] \"%s\"  <-->  \"%s\"\n",
+            i,
+            artists[duplicates[i, 1]],
+            artists[duplicates[i, 2]])
+  )
+}
+
+
+
+#Rembrandt 
+        
+
+
+
+# 2. Explore Data===================================================================================
+
 
 
 
