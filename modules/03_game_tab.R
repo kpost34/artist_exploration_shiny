a2f880da2e9d2d76f12ccdbbe49079fafdbb47d7
Keith <kpost342005@hotmail.com>
2025-10-26 17:16:06 -0400

    Added next round button that dynamically displays, modals that display information conditioned on difficulty, tabulation of correct answers for user and model, and a score table that takes on current round and total scores

--- modules/03_game_tab.R
+++ modules/03_game_tab.R
@@ -31,35 +31,40 @@ gameUI <- function(id) {
         #easy = 3 choices, normal = 4 choices, hard = 5 choices
       ),
       column(1),
-      column(2, 
+      column(1, 
         br(),
-        actionButton(ns("btn_diff"), "Submit")
+        actionButton(ns("btn_start_game"), "New Game")
+      ),
+      column(1,
+        br(),
+        uiOutput(ns("ui_btn_next_round"))
       )
     ),
             
     ## Delineation 
-    br(),
     hr(),
     
     ## Artwork, choices, and answer response
     fluidRow(
-      column(1),
-      1:5 %>%
-        purrr::map(build_q_a_block, id=id),
-      column(1)
+      purrr::map(1:5, build_q_a_block, id=id),
+      column(2,
+        DTOutput(ns("tab_game_score"))
+      )
     ),
     
     br(),
     
     ## Submit answers
     fluidRow(
-      column(5),
-      column(2,
-        uiOutput(ns("ui_btn_submit_art")),
-        br(),
+      column(4,
         textOutput(ns("txt_n_correct"))
       ),
-      column(5)
+      column(2,
+        uiOutput(ns("ui_btn_submit_art"))
+      ),
+      column(6,
+        textOutput(ns("txt_n_mod_correct"))
+      )
     )
   )
 }
@@ -67,17 +72,17 @@ gameUI <- function(id) {
 
 
 # Server============================================================================================
-gameServer <- function(id, mod, current_tab) {
+gameServer <- function(id, mod) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
-    ## Reactive DFs
+    ## Reactive DFs-------------------
     ### Hold shrinking DF of game artwork
     rv_remaining <- reactiveVal(df_game) #seed with initial DF
     
     
     ### Extract sample & retain remaining records
-    df_sample <- eventReactive(input$btn_diff, {
+    df_sample <- eventReactive(input$btn_start_game, {
       # req(current_tab()=="Game")
       
       df <- rv_remaining()
@@ -95,15 +100,6 @@ gameServer <- function(id, mod, current_tab) {
     })
     
     
-    ### Create reactive of correct answers with metadata
-    df_artist_meta <- reactive({
-      df <- df_modal %>%
-        filter(object_id %in% df_sample$object_id)
-      
-      df
-    })
-    
-    
     ### Create vector of wrong answers
     vec_wrong_artists <- reactive({
       df_game %>%
@@ -113,13 +109,7 @@ gameServer <- function(id, mod, current_tab) {
     })
     
     
-    ## Display blocks of UI
-    ### Pre-process images
-    input_ids <- paste0("input_answer_art", 1:5)
-    # vec_correct_answers <- c("Monet", "Dali", "Monet", "Manet")
-    vec_answers_msg <- paste0("txt_answer_msg_art", 1:5)
-    
-    
+    ## Images-------------------
     ### Render images
     observeEvent(df_sample(), {
       
@@ -144,9 +134,12 @@ gameServer <- function(id, mod, current_tab) {
     })
     
     
-    ### Modals
-    #### Display buttons to show modals
-    observeEvent(input$btn_diff, {
+    ## Modals-------------------
+    ### Display buttons to show modals
+    observeEvent(input$btn_start_game, {
+      #no modals on hard mode
+      req(input$sldT_diff %in% c("easy", "normal"))
+      
       1:5 %>%
         purrr::map(function(x) {
           #create outputs and inputs
@@ -162,30 +155,49 @@ gameServer <- function(id, mod, current_tab) {
     })
     
     
-    #### Render tables for modals
+    ### Render tables for modals
     observeEvent(df_sample(), {
+      #no modals on hard mode
+      req(input$sldT_diff %in% c("easy", "normal"))
+      
       purrr::map(1:5, function(x) {
         nm_dt_meta <- paste0("dt_art_meta", x)
+        
+        #dynamically choose labels (attribute names)
+        labs_modal_sel <- if(input$sldT_diff=="easy"){
+          labs_modal_easy
+        } else if(input$sldT_diff=="normal"){
+          labs_modal_normal
+        }
+        
+        #turn sample date into singular, transposed DFs
         df_meta <- extract_rotate_meta(df=df_sample(),
                                        row=x,
-                                       fields=vec_modal_easy)
-
-        output[[nm_dt_meta]] <- DT::renderDataTable({
-          DT::datatable(df_meta)
+                                       fields=labs_modal_sel)
+        
+        #render clean DTs of attribute
+        output[[nm_dt_meta]] <- DT::renderDT({
+          DT::datatable(df_meta,
+                        rownames=FALSE,
+                        options=list(dom="t",
+                                     ordering=FALSE)
+          )
         })
       })
     })
     
     
-    #### Display modals
+    ### Display modals
     purrr::map(1:5, function(x) {
       observeEvent(input[[paste0("btn_modal_art", x)]], {
+        #no modals on hard mode
+        req(input$sldT_diff %in% c("easy", "normal"))
+        
         nm_modal_table <- paste0("dt_art_meta", x)
         
         showModal(modalDialog(
-          title=paste("Modal for button", x),
-          DT::dataTableOutput(ns(nm_modal_table)),
-          # p(paste("You clicked button number", x, ". This is the modal for it.")),
+          title=paste("Artwork", x),
+          DT:DTOutput(ns(nm_modal_table)),
           footer=modalButton("Close"),
           easyClose=TRUE
         ))
@@ -193,8 +205,9 @@ gameServer <- function(id, mod, current_tab) {
     })
     
     
+    ## Answer choices and assessment-------------------
     ### Conditionally display answer (choices) UI
-    observeEvent(input$btn_diff, {
+    observeEvent(input$btn_start_game, {
       purrr::map(1:5, function(x) {
         #create outputs and inputs
         nm_input_answer <- paste0("input_answer_art", x)
@@ -232,22 +245,31 @@ gameServer <- function(id, mod, current_tab) {
     })
     
     
-    ### Conditionally display answer submit button
-    #### Create reactive of answers being selected
-    all_selected <- reactive({
+    ### Choice selection
+    #### User: Create reactive vector of choices selected
+    user_selected <- reactive({
+      input_ids <- paste0("input_answer_art", 1:5)
       #combine answers and replace default (NULL) with empty string
       map_chr(input_ids, ~ input[[.x]] %||% "")
     })
     
     
+    #### Model: Create reactive vector of choice selected
+    model_selected <- reactive({
+      predict(mod, df_sample()) %>%
+        .[[1]] %>%
+        as.character()
+    })
+    
+    
     ### Display submit button when all radio buttons are selected/text inputs have any characters
-    observeEvent(all_selected(), {
-      selected_answers <- all_selected()
+    observeEvent(user_selected(), {
+      selected_answers <- user_selected()
 
       #show button only all inputs have a selection or character
       if(all(nzchar(selected_answers))) {
         output$ui_btn_submit_art <- renderUI({
-          actionButton(ns("btn_submit_art"), "Submit answers")
+          actionButton(ns("btn_submit_art"), "Confirm selections")
         })
       } else {
         output$ui_btn_submit_art <- renderUI(NULL)  #hide if not all selected
@@ -255,43 +277,120 @@ gameServer <- function(id, mod, current_tab) {
     })
     
     
-    ## Evaluate whether answers are correct
-    ### Display number correct below submit button
-    #### Create reactive of each answer assessment
-    vec_answers_assess <- reactive({
+    ### Hide submit button once clicked
+    observeEvent(input$btn_submit_art, {
+      output$ui_btn_submit_art <- renderUI(NULL)
+      output$ui_btn_next_round <- renderUI({
+        actionButton(ns("btn_next_round"), "Next Round")
+      })
+    })
+    
+    
+    ### Evaluate whether individual answers are correct
+    #### User: Create reactive of each answer assessment
+    vec_user_answers_assess <- reactive({
       req(input$btn_submit_art)
-      all_selected()==df_sample()$artist_clean
+      user_selected()==df_sample()$artist_clean
+    })
+    
+    
+    #### Model: Create reactive of each answer assessment
+    vec_model_answers_assess <- reactive({
+      req(input$btn_submit_art)
+      model_selected()==df_sample()$artist_clean
     })
     
     
     #### Render output
     observeEvent(input$btn_submit_art, {
-      req(vec_answers_assess())
+      req(vec_user_answers_assess(),
+          vec_model_answers_assess()
+      )
+      
+      vec_answers_msg <- paste0("txt_answer_msg_art", 1:5)
+      vec_mod_answers <- paste0("txt_mod_answer_art", 1:5)
+      vec_mod_answers_msg <- paste0("txt_mod_answer_msg_art", 1:5)
+      vec_correct_answers <- paste0("txt_correct_answer", 1:5)
+      
+      intro_model <- c("Model selections:", rep("<br>", 4))
+      
+      intro_answer <- c("Correct answers:", rep("<br>", 4))
+      
       
       purrr::map(1:5, function(x) {
-        output[[vec_answers_msg[x]]] <- renderUI({
-          if(vec_answers_assess()[x]) {
-            HTML("<span style='color: green;'>Correct!</span>")
-          } else{
-            HTML("<span style='color: red;'>Wrong</span>")
-          }
+        #mod answers
+        output[[vec_mod_answers[x]]] <- renderUI({
+          
+          HTML(intro_model[x],
+          # HTML(paste0("<p>", intro_model[x], "</p>"),
+               paste0("<p>", model_selected()[x], "</p>"))
         })
-          # output[[vec_answers_msg[x]]] <- renderText({
-          #   vec_text_msg[x]
-          # })
+        
+        #correct answers
+        output[[vec_correct_answers[x]]] <- renderUI({
+          answer <- df_sample()$artist_clean[x]
+          
+          HTML(intro_answer[x],
+          # HTML(paste0("<p>", intro_answer[x], "</p>"),
+               paste0("<p><strong>", answer, "</strong></p>"))
+        })
+        
+        #user answer right/wrong
+        # output[[vec_answers_msg[x]]] <- renderUI({
+        #   if(vec_user_answers_assess()[x]) {
+        #     HTML("<span style='color: green;'>Correct!</span>")
+        #   } else{
+        #     HTML("<span style='color: red;'>Wrong</span>")
+        #   }
+        # })
+        
+        #mod answer right/wrong
+        # output[[vec_mod_answers_msg[x]]] <- renderUI({
+        #   if(vec_model_answers_assess()[x]) {
+        #     HTML("<span style='color: green;'>Correct!</span>")
+        #   } else{
+        #     HTML("<span style='color: red;'>Wrong</span>")
+        #   }
+        # })
       })
     })
     
     
-    ### Display correct/incorrect below each response/artwork
-    #### Create reactive
+    ### Count numbers of correct selections 
+    #### Create recent round reactives
     n_correct <- reactive({
-      req(vec_answers_assess())
-      sum(vec_answers_assess())
+      # req(vec_user_answers_assess())
+      sum(vec_user_answers_assess())
     })
     
     
-    #### Render output
+    n_mod_correct <- reactive({
+      # req(vec_model_answers_assess())
+      sum(vec_model_answers_assess())
+    })
+    
+    
+    #### Create total reactives
+    #seed with 0s
+    rv_user_score <- reactiveVal(0)
+    rv_mod_score <- reactiveVal(0)
+    
+    #get totals
+    observeEvent(input$btn_submit_art, {
+      # req(n_correct(), n_mod_correct())
+      #user
+      current_val <- rv_user_score()
+      rv_user_score(current_val + n_correct())
+      
+      #model
+      current_mod_val <- rv_mod_score()
+      rv_mod_score(n_mod_correct() + current_mod_val)
+    })
+    
+    
+    
+    #### Render outputs
+    ##### Text
     output$txt_n_correct <- renderText({
       req(n_correct())
       obj <- if(n_correct()==1){
@@ -301,7 +400,34 @@ gameServer <- function(id, mod, current_tab) {
       }
       paste("You identified", n_correct(), obj, "correctly")
     })
+  
+    output$txt_n_mod_correct <- renderText({
+      req(n_mod_correct())
+      obj <- if(n_mod_correct()==1){
+        "painting"
+      } else if(n_mod_correct()!=1){
+        "paintings"
+      }
+      paste("The model identified", n_mod_correct(), obj, "correctly")
+    })
     
+    
+    ##### Tabular
+    output$tab_game_score <- renderDT({
+      req(n_correct(), n_mod_correct())
+      
+      df_score <- tibble(
+        player=c("user", "model"),
+        round=c(n_correct(), n_mod_correct()),
+        total=c(rv_user_score(), rv_mod_score())
+      )
+      
+      DT::datatable(df_score,
+                    rownames=FALSE,
+                    options=list(dom="t",
+                                 ordering=FALSE)
+      )
+    })
     
   })
 }
